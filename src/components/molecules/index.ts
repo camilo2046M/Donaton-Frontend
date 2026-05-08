ff056a55b46c8bb44da46e9c960a4a1123463936
camilo2046M <ca.menav@duocuc.cl>
2026-05-08 18:26:30 -0400

    Añadidas moléculas para el diseño

---
+++ src/components/molecules/index.ts
@@ -0,0 +1,15 @@
+export { FormField }      from './FormField';
+export { StatCard }       from './StatCard';
+export { TableRow }       from './TableRow';
+export { NeedCard }       from './NeedCard';
+export { FallbackBanner } from './FallBackbanner';
+export { TabBar }         from './TabBar';
+export { ModalHeader }    from './ModalHeader';
+
+export type { FormFieldProps }      from './FormField';
+export type { StatCardProps, DeltaType } from './StatCard';
+export type { TableRowProps }       from './TableRow';
+export type { NeedCardProps }       from './NeedCard';
+export type { FallbackBannerProps } from './FallBackbanner';
+export type { TabBarProps, TabItem } from './TabBar';
+export type { ModalHeaderProps }    from './ModalHeader';
